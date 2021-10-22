d7eac928db031e4e88e03b9d4a1e0d8d4bca65fb
Augusto Mendonca <augusto.sousa@economia.gov.br>
2021-10-22 09:10:09 -0300

    concluido

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    # collection do
+      # get :top
+    # end
+
+    # member do
+    #  get :chef
+    # end
+
+  #   # NESTING:
+  #   # /restaurants/:restaurant_id/reviews
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
